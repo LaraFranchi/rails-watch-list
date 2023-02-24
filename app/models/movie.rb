795ad2ffc0c5145535dc59e2cd1c00604888cebe
Lara Franchi <laradeandrade@protonmail.com>
2023-02-24 15:33:32 -0500

    RailsWatchList

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,11 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
 
 
+# title is unique (FAILED - 16)
+# title cannot be blank (FAILED - 17)
+# overview cannot be blank (FAILED - 18)
